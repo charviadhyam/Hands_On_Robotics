26f71cab15c45ff8e6cde778c70c9a9ade31d1fd
charviadhyam <charvi.adhyam2024@vitstudent.ac.in>
2025-09-20 23:20:23 +0530

    Update POTENTIOMETER.py

--- POTENTIOMETER.py
+++ POTENTIOMETER.py
@@ -6,9 +6,7 @@ pot = ADC(Pin(34))            # Potentiometer connected to GPIO34 (ADC input)
 pot.atten(ADC.ATTN_11DB)      # Full range: 0–3.3V (ESP32)
 pot.width(ADC.WIDTH_12BIT)    # 12-bit resolution: 0–4095
 
-
 while True:
     pot_value = pot.read()            # Read potentiometer (0–4095)
-    duty = int((pot_value / 4095) * 1023)  # Map to PWM duty (0–1023)
-    print("Pot:", pot_value, "Duty:", duty)
+    print("Pot:", pot_value)
     time.sleep(0.05)
