a85b836ccd79f93d3cd3761b7c6c7e651da82f8d
Anshuman Singh <anshumansingh0106@gmail.com>
2025-09-21 21:30:10 +0530

    Update POTENTIOMETER.py

--- POTENTIOMETER.py
+++ POTENTIOMETER.py
@@ -2,7 +2,7 @@ from machine import Pin, ADC, PWM
 import time
 
 # Define pins
-pot = ADC(Pin(34))            # Potentiometer connected to GPIO34 (ADC input)
+pot = ADC(Pin(32))            # Potentiometer connected to GPIO32 (ADC input)
 pot.atten(ADC.ATTN_11DB)      # Full range: 0–3.3V (ESP32)
 pot.width(ADC.WIDTH_12BIT)    # 12-bit resolution: 0–4095
 
