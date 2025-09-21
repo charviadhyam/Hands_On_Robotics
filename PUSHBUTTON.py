06709af4ff4092359871e737ee751a98f40e9aee
Anshuman Singh <anshumansingh0106@gmail.com>
2025-09-21 21:41:41 +0530

    Update PUSHBUTTON.py

--- PUSHBUTTON.py
+++ PUSHBUTTON.py
@@ -1,8 +1,8 @@
 from machine import Pin
 from time import sleep
 
-button = Pin(14, Pin.IN, Pin.PULL_DOWN)   # Button pin with pull-down resistor
-led = Pin(5, Pin.OUT)                    # LED pin
+button = Pin(21, Pin.IN, Pin.PULL_UP)   # Button pin with pull-down resistor
+led = Pin(22, Pin.OUT)                    # LED pin
 
 while True:
     if button.value() == 0:   # Button pressed
