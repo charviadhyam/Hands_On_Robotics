aa4fc0c82d3ef5edfef0d41c005f7601c17cb094
charviadhyam <charvi.adhyam2024@vitstudent.ac.in>
2025-09-21 13:08:43 +0530

    Update LED.py

--- LED.py
+++ LED.py
@@ -5,7 +5,7 @@ import time
 led = Pin(18, Pin.OUT)   
 
 while True:
-    led.on()          # LED ON
-    time.sleep(1)     # Wait 1 second
-    led.off()         # LED OFF
-    time.sleep(1)     # Wait 1 second
+    led.value(1)    # Led turns on
+    sleep(1)    # Wait 1 second
+    led.value(0)    # Led turns off
+    sleep(1)    # Wait 1 second
