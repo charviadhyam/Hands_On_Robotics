2fb656affdfeaff2217e7d4ff35fd6f9ea649864
Anshuman Singh <anshumansingh0106@gmail.com>
2025-09-21 21:40:02 +0530

    Update POTENTIOMETER.py

--- POTENTIOMETER.py
+++ POTENTIOMETER.py
@@ -1,12 +1,12 @@
-from machine import Pin, ADC, PWM
-import time
+from machine import Pin, ADC
+from time import sleep
 
 # Define pins
 pot = ADC(Pin(32))            # Potentiometer connected to GPIO32 (ADC input)
 pot.atten(ADC.ATTN_11DB)      # Full range: 0–3.3V (ESP32)
-pot.width(ADC.WIDTH_12BIT)    # 12-bit resolution: 0–4095
+  # 12-bit resolution: 0–4095
 
 while True:
-    pot_value = pot.read()            # Read potentiometer (0–4095)
-    print("Pot:", pot_value)
-    time.sleep(0.05)
+    value = pot.read()            # Read potentiometer (0–4095)
+    print("Pot: ", value)
+    sleep(0.5)
